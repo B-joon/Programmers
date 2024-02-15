63dbb3c67623c89441f6d136aaa8730ce8a32116
B-joon <75296934+B-joon@users.noreply.github.com>
2024-02-15 19:20:17 +0900

    [level 4] Title: 년, 월, 성별 별 상품 구매 회원 수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131532. 년， 월， 성별 별 상품 구매 회원 수 구하기/README.md
+++ 프로그래머스/4/131532. 년， 월， 성별 별 상품 구매 회원 수 구하기/README.md
@@ -1,6 +1,6 @@
 # [level 4] 년, 월, 성별 별 상품 구매 회원 수 구하기 - 131532 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131532) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131532?language=oracle) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@ Empty
 
 ### 제출 일자
 
-2024년 2월 6일 17:32:39
+2024년 2월 4일 19:20:15
 
 ### 문제 설명
 
--- 프로그래머스/4/131532. 년， 월， 성별 별 상품 구매 회원 수 구하기/년， 월， 성별 별 상품 구매 회원 수 구하기.sql
+++ 프로그래머스/4/131532. 년， 월， 성별 별 상품 구매 회원 수 구하기/년， 월， 성별 별 상품 구매 회원 수 구하기.sql
@@ -1,7 +1,21 @@
 -- 코드를 입력하세요
-select year(a.sales_date) as year, month(a.sales_date) as moth, 
-b.gender, count(distinct a.user_id) as users from online_sale a
-join user_info b on a.user_id = b.user_id
-where b.gender is not null
-group by year(a.sales_date), month(a.sales_date), b.gender
-order by year(a.sales_date), month(a.sales_date), b.gender
+-- SELECT to_char(b.sales_date, 'YYYY') as year, 
+-- to_char(b.sales_date, 'DD') as month,
+-- a.gender,
+-- count(user_id) as users
+-- from user_info a
+-- inner join online_sale b
+-- where a.gender is not null
+-- group by to_char(b.sales_date, 'YYYY'), to_char(b.sales_date, 'DD'), a.gender
+-- order by to_char(b.sales_date, 'YYYY'), to_char(b.sales_date, 'DD'), a.gender
+
+select extract(year from b.sales_date) as year,
+extract(month from b.sales_date) as month,
+a.gender,
+count(distinct a.user_id) as users
+from user_info a
+inner join online_sale b
+on a.user_id = b.user_id
+where a.gender is not null
+group by extract(year from b.sales_date), extract(month from b.sales_date), a.gender
+order by extract(year from b.sales_date), extract(month from b.sales_date), a.gender
